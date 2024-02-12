b49e70383f5cf8bded5ca50536121d92e71576d1
paveltretyakov94@gmail.com <paveltretyakov94@gmail.com>
2024-02-12 23:32:26 +0400

    Operation with multiple processes

--- IntroductionToSP/Processes/Form1.cs
+++ IntroductionToSP/Processes/Form1.cs
@@ -7,27 +7,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Processes
 {
     public partial class Form1 : Form
     {
+        List<Process> processList;
+
         public Form1()
         {
             InitializeComponent();
-            InitProcess();
+
+            processList = new List<Process>();
+            //InitProcess();
         }
 
         void Form1_Closing(object sender, CancelEventArgs e) 
         {
-            myProcess.CloseMainWindow();
-            myProcess.Close();
+            foreach (Process item in processList)
+            {
+                if (!item.HasExited)
+                {
+   
+                    item.CloseMainWindow();
+                    item.Close();
+                }
+            }
+            processList.Clear();
         }
 
         void InitProcess()
         {
             AlignText();
+            myProcess = new Process();
             myProcess.StartInfo = new System.Diagnostics.ProcessStartInfo(richTextBoxProcessName.Text);
+
+            processList.Add(myProcess);
         }
 
         void AlignText()
@@ -45,23 +61,43 @@ namespace Processes
 
         private void buttonStop_Click(object sender, EventArgs e)
         {
-            myProcess.CloseMainWindow(); //закрывает процесс
-            myProcess.Close();          //освобождает ресурсы, занимаемые процессом
+            if (processList.Count > 0) 
+            {
+                if (!myProcess.HasExited)
+                {
+                    myProcess.CloseMainWindow(); //закрывает процесс
+                    myProcess.Close();          //освобождает ресурсы, занимаемые процессом
+                }
+                processList.RemoveAt(processList.Count - 1);
+            }
+
+            if (processList.Count > 0)
+                myProcess = processList[processList.Count - 1];
+
+            Info();
         }
 
         void Info()
         {
-            labelProcessInfo.Text = "Process info:\n";
-            labelProcessInfo.Text += $"PID:                     {myProcess.Id}\n";
-            labelProcessInfo.Text += $"Base priority:           {myProcess.BasePriority}\n";
-            labelProcessInfo.Text += $"Priority Class:          {myProcess.PriorityClass}\n";
-            labelProcessInfo.Text += $"Start time:              {myProcess.StartTime}\n";
-            labelProcessInfo.Text += $"Total Processor Time:    {myProcess.TotalProcessorTime}\n";
-            labelProcessInfo.Text += $"User Processor Time:     {myProcess.UserProcessorTime}\n";
-            labelProcessInfo.Text += $"Session Id:              {myProcess.SessionId}\n";
-            labelProcessInfo.Text += $"Name:                    {myProcess.ProcessName}\n";
-            labelProcessInfo.Text += $"Processor Affinity:      {myProcess.ProcessorAffinity}\n";
-            labelProcessInfo.Text += $"Threads:                 {myProcess.Threads.Count}\n";
+            if (processList.Count > 0)
+            {
+                labelProcessInfo.Text = $"Total process count: {processList.Count}\n";
+                labelProcessInfo.Text += "Process info:\n";
+                labelProcessInfo.Text += $"PID:                     {myProcess.Id}\n";
+                labelProcessInfo.Text += $"Base priority:           {myProcess.BasePriority}\n";
+                labelProcessInfo.Text += $"Priority Class:          {myProcess.PriorityClass}\n";
+                labelProcessInfo.Text += $"Start time:              {myProcess.StartTime}\n";
+                labelProcessInfo.Text += $"Total Processor Time:    {myProcess.TotalProcessorTime}\n";
+                labelProcessInfo.Text += $"User Processor Time:     {myProcess.UserProcessorTime}\n";
+                labelProcessInfo.Text += $"Session Id:              {myProcess.SessionId}\n";
+                labelProcessInfo.Text += $"Name:                    {myProcess.ProcessName}\n";
+                labelProcessInfo.Text += $"Processor Affinity:      {myProcess.ProcessorAffinity}\n";
+                labelProcessInfo.Text += $"Threads:                 {myProcess.Threads.Count}\n"; 
+            }
+            else
+            {
+                labelProcessInfo.Text = "Нет запущенных процессов";
+            }
         }
     }
 }
